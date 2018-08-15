cc3a618ace9ee208c13bafb15d41c608aa8194c5
Conrad <conradkay123@gmail.com>
2018-08-14 21:02:58 -0400

    update reducer snapshot

--- src/addTodo/addTodo.spec.js
+++ src/addTodo/addTodo.spec.js
@@ -1,23 +1,21 @@
 //@flow
 import React from 'react'
 import {AddTodo} from './addTodo.jsx'
-import {createStore} from 'redux'
-import { mount } from 'enzyme'
-import type {Action, State} from '../types/basic'
+import { shallow } from 'enzyme'
 describe('addTodo component', () => {
-  const store = createStore((action: Action, state: State) => {return state})
   const spy = jest.fn()
-  const wrap = mount(
-    <AddTodo store={store} addTodo={spy} />
+  const wrap = shallow(
+    <AddTodo addTodo={spy} />
   )
-  it('should change input text and dispatch addtodo action with input name when button is clicked', () => {
-    const button = wrap.find('button')
-    const input = wrap.find('input')
-    button.simulate('click')
-    expect(spy).toHaveBeenCalled()
-    input.simulate('keydown', { which: 'a' })
-  })
   it('should match snapshot', () => {
     expect(wrap).toMatchSnapshot()
+  })
+  it('should change input text and dispatch addtodo action with input name when button is clicked', () => {
+    const button = wrap.find('button')
+    button.simulate('click')
+    const input = wrap.find('input')
+    input.simulate('keydown', { which: 'a' })
+    expect(spy).toHaveBeenCalled()
+    expect(input.props().value).toBe('namea')
   })
 })
